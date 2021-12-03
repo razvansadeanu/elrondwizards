dc6572663e09d9b6e7580930a969656e17c1f2ee
Razvan Sadeanu <razvan.sadeanu@gmail.com>
2021-12-03 11:28:17 +0200

    Create About template

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,25 @@ import video from "../static/Video/test2.mp4";
 
 function About() {
   return (
-    <div className="aboutContainer">
-      <video src={video} playsInline autoPlay loop muted></video>
-      <div class="caption">{/* <h2>Your caption here</h2> */}</div>
-    </div>
+    <>
+      <div className="aboutContainer">
+        <video src={video} playsInline autoPlay loop muted></video>
+        <div class="caption">
+          <div className="titleText">
+            <span></span>LEGEND OF...
+          </div>
+        </div>
+        <div className="aboutText">
+          <p>
+            Once upon a time in a magical forest, the legend says that in this
+            mystical place 3 High Wizards ruled the land and created 3333
+            magical keys, keys that have the power to change the future and were
+            hidden away so that only a few chosen souls would be able to get
+            them when the time was right.
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
